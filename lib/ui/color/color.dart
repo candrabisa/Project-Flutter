ebba91647525c95e96808d9f471541e81b49c1fe
candraseng <candrabillysagita@gmail.com>
2020-04-29 22:23:27 +0700

    no message

---
+++ lib/ui/color/color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LightColors  {
+  static const Color bakground = Color(0xFFFFF9EC);
+  
+  static const Color kDarkYellow = Color(0xFFF9BE7C);
+  
+  static const Color kRed = Color(0xFFE46472);
+  
+  static const Color kBlue = Colors.blue;
+  
+  static const Color kGreen = Colors.green;
+
+  static const Color abuabu = Color(0xFF0D253F);
+  static const Color kivy = Colors.purple;
+
+}
